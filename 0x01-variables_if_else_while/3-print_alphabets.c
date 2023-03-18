f797cc6526747ea16769ba6472c6303fb6875e0b
Hagerezz <queen.emilly12@gmail.com>
2023-03-17 19:00:34 -0700

    c-variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphapet in lowercase, and then in uppercase,
+ * follewed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}

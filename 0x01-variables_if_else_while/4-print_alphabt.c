3cfe0ac187e6834e62b7853eaaa78a45020645fa
Hagerezz <queen.emilly12@gmail.com>
2023-03-17 19:11:31 -0700

    c-variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphapet in lowercase,
+ * follewed by a new line, except q and e
+ * Return Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

5e3d4ec4ff240c4fb8b36a795f1081971570b012
Hagerezz <queen.emilly12@gmail.com>
2023-03-18 18:14:34 -0700

    variables

---
+++ 0x00-hello_world/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ * follewed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}

87cbb382b7df975c270380101d26cb7702c9e577
Hagerezz <queen.emilly12@gmail.com>
2023-03-18 18:36:39 -0700

    variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	char n;
+	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-		if (n != '9')
+		if (n != 57)
 		{
 			putchar(',');
 			putchar(' ');

a3123157caa03bb9095957d257440f1c67fe0ba0
Hagerezz <queen.emilly12@gmail.com>
2023-03-27 17:22:46 -0700

    hello

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-/** 
+/**
  * print_array - prints n elements of an array of integers
  * @a: array name
  * @n: is the number of elements of the array to be printed

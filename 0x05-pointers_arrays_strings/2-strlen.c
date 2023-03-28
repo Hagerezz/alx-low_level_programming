002b087e7b26326ce83198426954291d89ae95d9
Hagerezz <queen.emilly12@gmail.com>
2023-03-27 18:02:18 -0700

    hello

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	char ch = s[0];
+	int n = 0;
+	
+	while (ch != '\0')
+		ch = s[++n];
+	return (n);
+}

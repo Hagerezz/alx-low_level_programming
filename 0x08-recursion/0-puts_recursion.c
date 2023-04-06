d696c0b25804e6160f6e05f0bbc984bff469f443
Hagerezz <queen.emilly12@gmail.com>
2023-04-06 16:34:46 -0700

    hello

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,4 +2,20 @@
 #include <stdio.h>
 
 /**
- *
+ *_puts_recursion - prints a string, followed by a new line
+ * @s: input
+ * Return: Always 0 (success)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_put_recursion(s+1);
+	}
+	else 
+	{
+		_putchar('\n');
+	}
+}

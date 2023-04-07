c241739079f8cf6aff6fdf548d0280bd69f60ea9
Hagerezz <queen.emilly12@gmail.com>
2023-04-06 18:05:00 -0700

    hello

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,13 +8,21 @@
  * Return: 0 (if n < 0)
  */
 
+/**
+ * is_prime - returns 1 if input is prime
+ * @a: input
+ * @b: input
+ * @n: input
+ * Return: 0 (if n is not prime
+ */
+
 int is_prime(int a, int b)
 {
-        if (b * b > a)
-                return (1);
-        if (a % b == 0)
-                return (0);
-        return is_prime(a, b + 1);
+	if (b * b > a)
+		return (1);
+	if (a % b == 0)
+		return (0);
+	return (is_prime(a, b + 1));
 }
 
 int is_prime_number(int n)
@@ -23,5 +31,5 @@ int is_prime_number(int n)
 	{
 		return (0);
 	}
-	return is_prime(n, 2);
+	return (is_prime(n, 2));
 }

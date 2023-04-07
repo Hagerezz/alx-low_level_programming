26c2cf6fcd615ae895b3e05c22c4b4538ae03f69
Hagerezz <queen.emilly12@gmail.com>
2023-04-06 18:25:19 -0700

    hello

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 /**
- * sqrt - returns -1 if not complete square
+ * sqt - returns -1 if not complete square
  * else returns the value of the square root
  * @a: input
  * @b: input
  * Return: integer
  */
 
-int sqrt(int a, int b)
+int sqt(int a, int b)
 {
 	if (b * b == a)
 		return (b);
 	if (b * b > a)
 		return (-1);
-	return (sqrt(a, b + 1));
+	return (sqt(a, b + 1));
 }
 
 /**
@@ -28,5 +28,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (sqrt(n, 0));
+	return (sqt(n, 0));
 }

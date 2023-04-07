dd9c3ce59807a2db3cfe040d9e10c3780e08214b
Hagerezz <queen.emilly12@gmail.com>
2023-04-06 18:13:22 -0700

    hello

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,13 +2,6 @@
 #include <stdio.h>
 
 /**
- * is_prime_number - returns 1 if the input integer is a prime number
- * otherwise return 0
- * @n: input
- * Return: 0 (if n < 0)
- */
-
-/**
  * is_prime - calculates if a number is prime recursively
  * @a: input
  * @b: input
@@ -24,6 +17,13 @@ int is_prime(int a, int b)
 		return (0);
 	return (is_prime(a, b + 1));
 }
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ * otherwise return 0
+ * @n: input
+ * Return: 0 (if n < 0)
+ */
 
 int is_prime_number(int n)
 {

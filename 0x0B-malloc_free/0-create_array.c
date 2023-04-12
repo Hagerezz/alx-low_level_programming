b045e765164b6a4345b6af33acbd0298979dbd30
Hagerezz <queen.emilly12@gmail.com>
2023-04-11 18:24:42 -0700

    hello

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,12 +10,13 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *a;
+	unsigned int i;
+
 	if (size == 0)
-	{
 		return (NULL);
-	}
-	char* a;
 	a = malloc(size * sizeof(char));
-	a[0] = c;
+	for (i = 0; i < size; i++)
+		a[i] = c;
 	return (a);
 }

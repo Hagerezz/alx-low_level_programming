9dff7449beecf4d0ee2018dcf10be70c535a258c
Hagerezz <queen.emilly12@gmail.com>
2023-04-11 19:29:52 -0700

    hello

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *conct;
 	int i, ci;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -28,8 +29,8 @@ char *str_concat(char *s1, char *s2)
 	{
 		conct[i] = s1[i];
 		i++;
-	}	
-	while (s2[ci] != '\0')	
+	}
+	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
 		i++, ci++;

bad819e580a807080fabf70be45900ff381d1550
Hagerezz <queen.emilly12@gmail.com>
2023-04-13 18:59:22 -0700

    ahmed

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,11 +17,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 		return ((void *)malloc(new_size));
 	if (new_size == 0)
+	{
+		free(ptr);
 		return (NULL);
+	}
 	if (new_size == old_size)
 		return (ptr);
 	h = (char *)malloc(new_size);
 	for (i = 0; i < old_size && i < new_size; i++)
 		h[i] = ((char *)ptr)[i];
+	free(ptr);
 	return ((void *)h);
 }

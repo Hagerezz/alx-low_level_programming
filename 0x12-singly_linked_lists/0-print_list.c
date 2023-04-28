a743a3e5b17fe53488dc9ddadc71c2368991c54b
hagerezz <queen.emilly12@gmail.com>
2023-04-27 19:17:06 -0700

    hi

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,10 +11,9 @@ size_t print_list(const list_t *h)
 {
 	if (h == NULL)
 		return (0);
-
 	if (h->str == NULL)
 		printf("[0] (nil)\n");
 	else
 		printf("[%u] %s\n", h->len, h->str);
-	return (1 + print_list(h->next));
+	return (1 +print_list(h->next));
 }

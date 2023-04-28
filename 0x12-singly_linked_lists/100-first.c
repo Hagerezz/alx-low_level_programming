4b7e42dacd6d5c7b3049683938570fa49e1ab4fb
hagerezz <queen.emilly12@gmail.com>
2023-04-27 18:35:09 -0700

    hi

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+void print(void)__attribute__((constructor));
+/**
+ * print - print before the main function
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+		I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: list to be free
+ */
+void free_list(list_t *head)
+{
+	
+}

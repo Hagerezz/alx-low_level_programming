7ac4b6602036f05e75bfba23f6196d858bd92be7
hagerezz <queen.emilly12@gmail.com>
2023-05-02 16:16:28 -0700

    ahmed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
+void free_list(listint_t *h);
 /**
  * free_listint2 - frees a list
  * @head: pointer
@@ -14,7 +16,7 @@ void free_listint2(listint_t **head)
 
 void free_list(listint_t *h)
 {
-	if (h != NULL)
+	if (h == NULL)
 	{
 		free_list(h->next);
 		free(h);

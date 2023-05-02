5b777e287f8b0debe1f1bdf38ba89ccdff763995
hagerezz <queen.emilly12@gmail.com>
2023-05-02 16:11:26 -0700

    ahmed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,6 +8,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	free_listint(*head);
+	free_list(*head);
 	free(head);
+}
+
+void free_list(listint_t *h)
+{
+	if (h != NULL)
+	{
+		free_list(h->next);
+		free(h);
+	}
 }

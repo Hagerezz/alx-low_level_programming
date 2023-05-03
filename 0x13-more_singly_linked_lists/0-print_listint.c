9f86764b399c116cd13986d2e17a4fc4de09d313
hagerezz <queen.emilly12@gmail.com>
2023-05-02 18:20:00 -0700

    ahmed

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t size = 0;
+	size_t size;
 
+	size = 0;
 	while (h != NULL)
 	{
-		size++; 
+		size++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}

bbe81f202f1aa548215a52ca94098bfe2a69490c
hagerezz <queen.emilly12@gmail.com>
2023-05-04 16:45:04 -0700

    ahmed is better then hager

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int b = 1;
+
+	if (n == 0 || n == 1)
+		printf("%ld\n, n");
+	while (n >= 1 << b)
+		b++;
+	b--;
+	while (b >= 0)
+	{
+		if (n - 1 << b >= 0)
+		{
+			printf("1");
+			n -= 1 << b
+		}
+		else
+			printf("0");
+		b--;
+	}
+	printf("\n");
+}

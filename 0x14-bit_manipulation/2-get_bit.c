85fd34fc3fecf4518673cadb34c1970f1b8bf928
hagerezz <queen.emilly12@gmail.com>
2023-05-04 17:59:48 -0700

    ahmed is better then hager

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,9 +5,11 @@
  * get_bit - returns the value of a bit at a given index
  * @n: number
  * @index: is the index, starting from 0 of the bit you want to get
- * Return: the value of the bit at index index or -1 if an error occured
+ * Return: the value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return ((n >> index) & 1);	
+	if (index > 63)
+		return (-1);
+	return ((n >> index) & 1);
 }

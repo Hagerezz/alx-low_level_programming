ce161019e906503bac27ed428332069aa9c78eb3
hagerezz <queen.emilly12@gmail.com>
2023-05-12 08:59:13 -0700

    hi

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 /**
  * create_file - creates a file
@@ -9,5 +10,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	
+	int file;
+	ssize_t b;
+	size_t len = strlen(text_content);
+
+	if (filename == NULL)
+		return(-1);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC,  S_IRUSR | S_IWUSR);
+	if (file == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		b = write(file, text_content, len);
+		if (b == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (-1);
 }

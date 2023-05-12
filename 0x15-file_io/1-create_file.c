3a010a385489c1b9d2617268b3631868ae420287
hagerezz <queen.emilly12@gmail.com>
2023-05-12 09:04:55 -0700

    hi

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,9 +4,9 @@
 #include "main.h"
 /**
  * create_file - creates a file
- * @filename - name of the file to create
- * @text_content - NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return(-1);
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC,  S_IRUSR | S_IWUSR);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file == -1)
 		return (-1);
 	if (text_content != NULL)
@@ -29,5 +29,5 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 	close(file);
-	return (-1);
+	return (1);
 }

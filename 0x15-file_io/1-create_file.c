739302e828858089cecd912598db393c8fb78d68
hagerezz <queen.emilly12@gmail.com>
2023-05-12 09:28:53 -0700

    hi

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,10 +15,12 @@ int create_file(const char *filename, char *text_content)
 	ssize_t len = strlen(text_content);
 
 	if (!filename)
+	{
 		return (-1);
+	}
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file == -1)
-		return (-1);
+		return (1);
 	if (text_content != NULL)
 	{
 		b = write(file, text_content, len);

3b8c875ab6d9572b2c7dbc11327399f75b1dc06f
Hagerezz <queen.emilly12@gmail.com>
2023-05-25 17:44:02 -0700

    h

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,50 +10,52 @@
  */
 int main(int argc, char *argv[])
 {
-    ssize_t a;
-    int f = 0;
-    int t = 0;
-    char b[1024];
+	ssize_t b;
+	int from = 0;
+	int to = 0;
+	char buf[1024];
 
-    if (argc != 3)
-        dprintf(STDERR_FILENO, USE);
-    exit(97);
-    f = open(argv[1], O_RDONLY);
-    if (f == -1)
-    {
-        dprintf(STDERR_FILENO, READ, argv[1]);
-        exit(98);
-    }
-    t = open(argv[2], PER, PERS);
-    if (t == -1)
-    {
-        dprintf(STDERR_FILENO, WRITE, argv[2]);
-        exit(99);
-    }
-    while ((a = read(f, b, 1024)) > 0)
-    {
-        if (write(t, b, a) != a)
-        {
-            dprintf(STDERR_FILENO, WRITE, argv[2]);
-            exit(99);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, USE);
+		exit(97);
 	}
-    }
-	if (a == -1)
+	from = open(argv[1], O_RDONLY);
+	if (f == -1)
+	{
+		dprintf(STDERR_FILENO, READ, argv[1]);
+		exit(98);
+	}
+	to = open(argv[2], PER, PERS);
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, WRITE, argv[2]);
+		exit(99);
+	}
+	while ((b = read(f, b, 1024)) > 0)
+	{
+		if (write(t, buf, b) != b)
+		{
+			dprintf(STDERR_FILENO, WRITE, argv[2]);
+			exit(99);
+		}
+	}
+	if (b == -1)
 	{
 		dprintf(STDERR_FILENO, READ, argv[2]);
 		exit(98);
 	}
-	f = close(f);
-	t = close(t);
-	if (f)
+	from = close(from);
+	to = close(to);
+	if (from)
 	{
-		dprintf(STDERR_FILENO, CLOSE, f);
+		dprintf(STDERR_FILENO, CLOSE, from);
 		exit(100);
 	}
-	if (t)
+	if (to)
 	{
-	dprintf(STDERR_FILENO, CLOSE, f);
-	exit(100);
+		dprintf(STDERR_FILENO, CLOSE, from);
+		exit(100);
 	}
-	return (EXIT_SUCCESS);
+	return (0);
 }

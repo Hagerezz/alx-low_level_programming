8d7f720abd9094a5dedea14b82edca9c0f926471
Hagerezz <queen.emilly12@gmail.com>
2023-05-25 18:18:33 -0700

    h

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,52 +10,52 @@
  */
 int main(int argc, char *argv[])
 {
-        ssize_t b;
-        int from = 0;
-        int to = 0;
-        char buf[1024];
+	ssize_t b;
+	int from = 0;
+	int to = 0;
+	char buf[1024];
 
-        if (argc != 3)
-        {
-                dprintf(STDERR_FILENO, USE);
-                exit(97);
-        }
-        from = open(argv[1], O_RDONLY);
-        if (from == -1)
-        {
-                dprintf(STDERR_FILENO, READ, argv[1]);
-                exit(98);
-        }
-        to = open(argv[2], PER, PERS);
-        if (to == -1)
-        {
-                dprintf(STDERR_FILENO, WRITE, argv[2]);
-                exit(99);
-        }
-        while ((b = read(from, buf, 1024)) > 0)
-        {
-                if (write(to, buf, b) != b)
-                {
-                        dprintf(STDERR_FILENO, WRITE, argv[2]);
-                        exit(99);
-                }
-        }
-        if (b == -1)
-        {
-                dprintf(STDERR_FILENO, READ, argv[1]);
-                exit(98);
-        }
-        from = close(from);
-        to = close(to);
-        if (from)
-        {
-                dprintf(STDERR_FILENO, CLOSE, from);
-                exit(100);
-        }
-        if (to)
-        {
-                dprintf(STDERR_FILENO, CLOSE, from);
-                exit(100);
-        }
-        return (0);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, USE);
+		exit(97);
+	}
+	from = open(argv[1], O_RDONLY);
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO, READ, argv[1]);
+		exit(98);
+	}
+	to = open(argv[2], PER, PERS);
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, WRITE, argv[2]);
+		exit(99);
+	}
+	while ((b = read(from, buf, 1024)) > 0)
+	{
+		if (write(to, buf, b) != b)
+		{
+			dprintf(STDERR_FILENO, WRITE, argv[2]);
+			exit(99);
+		}
+	}
+	if (b == -1)
+	{
+		dprintf(STDERR_FILENO, READ, argv[1]);
+		exit(98);
+	}
+	from = close(from);
+	to = close(to);
+	if (from)
+	{
+		dprintf(STDERR_FILENO, CLOSE, from);
+		exit(100);
+	}
+	if (to)
+	{
+		dprintf(STDERR_FILENO, CLOSE, from);
+		exit(100);
+	}
+	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,7 @@
 #define USE "Usage: cp file_from file_to\n"
 #define READ "Error: Can't read from file %s\n"
 #define WRITE "Error: Can't write to %s\n"
-#define CLOSE "Can't close fd %d\n"
+#define CLOSE "Error: Can't close fd %d\n"
 #define PER (O_WRONLY | O_CREAT | O_TRUNC)
 #define PERS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 

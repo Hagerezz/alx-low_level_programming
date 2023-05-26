7b106818602395265d35cb8244b9773155a7afcf
Hagerezz <queen.emilly12@gmail.com>
2023-05-25 17:24:38 -0700

    h

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,49 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define USE "Usage: cp file_from file_to\n"
-#define READ "Error: Can't read from file %s\n"
-#define WRITE "Error: Can't write to %s\n"
-#define CLOSE "Can't close fd %d\n"
-#define PER (O_WRONLY | O_CREAT | O_TRUNC)
-#define PERS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |S_IROTH)
 /**
  * main - copy
- * @v: pointer
- * @c: integer
+ * @argv: pointer
+ * @argc: integer
  * Return: 1 (success)
  */
-int main(int c, char **v)
+int main(int argc, char *argv[])
 {
-	ssize_t a;
-	int f = 0;
-	int t = 0;
-	char b[1024];
+    ssize_t a;
+    int f = 0;
+    int t = 0;
+    char b[1024];
 
-	if (c != 3)
-		dprintf(STDERR_FILENO, USE);
-		exit(97);
-	f = open(v[1], O_RDONLY);
-	if (f == -1)
-	{
-		dprintf(STDERR_FILENO, READ, v[1]);
-		exit(98);
+    if (argc != 3)
+        dprintf(STDERR_FILENO, USE);
+    exit(97);
+    f = open(argv[1], O_RDONLY);
+    if (f == -1)
+    {
+        dprintf(STDERR_FILENO, READ, argv[1]);
+        exit(98);
+    }
+    t = open(argv[2], PER, PERS);
+    if (t == -1)
+    {
+        dprintf(STDERR_FILENO, WRITE, argv[2]);
+        exit(99);
+    }
+    while ((a = read(f, b, 1024)) > 0)
+    {
+        if (write(t, b, a) != a)
+        {
+            dprintf(STDERR_FILENO, WRITE, argv[2]);
+            exit(99);
 	}
-	t = open(v[2], PER, PERS);
-	if (t == -1)
-	{
-		dprintf(STDERR_FILENO, WRITE, v[2]);
-		exit(99);
-	}
-	while ((a = read(f, b, 1024)) > 0)
-		if (write(t, b, a) != a)
-		{
-			dprintf(STDERR_FILENO, WRITE, v[2]);
-			exit(99);
-		}
+    }
 	if (a == -1)
 	{
-		dprintf(STDERR_FILENO, READ, v[2]);
+		dprintf(STDERR_FILENO, READ, argv[2]);
 		exit(98);
 	}
 	f = close(f);
@@ -56,8 +52,8 @@ int main(int c, char **v)
 	}
 	if (t)
 	{
-		dprintf(STDERR_FILENO, CLOSE, f);
-		exit(100);
+	dprintf(STDERR_FILENO, CLOSE, f);
+	exit(100);
 	}
 	return (EXIT_SUCCESS);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,13 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define USE "Usage: cp file_from file_to\n"
+#define READ "Error: Can't read from file %s\n"
+#define WRITE "Error: Can't write to %s\n"
+#define CLOSE "Can't close fd %d\n"
+#define PER (O_WRONLY | O_CREAT | O_TRUNC)
+#define PERS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);

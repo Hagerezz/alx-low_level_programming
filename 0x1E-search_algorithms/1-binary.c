d8f8fdb2392f32bf37b32cd780425c0f1bf096f7
hagerezz <queen.emilly12@gmail.com>
2024-02-08 00:52:50 +0200

    hi

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,30 +9,30 @@
 */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t n;
-    size_t i, j = 0;
+	size_t n;
+	size_t i, j = 0;
 
-    while (size > 0 && array)
-    {
-        n = (size - 1) / 2;
-        printf("Searching in array: ");
-        for (i = 0; i < size - 1; i++)
-        {
-            printf("%d, ", array[i]);
-        }
-        printf("%d\n", array[size - 1]);
-        if (value == array[n])
-            return (n + j);
-        else if (value < array[n])
-        {
-            size = n;
-        }
-        else if (value > array[n])
-        {
-            j += n + 1;
-            size = size - n - 1;
-            array += n + 1;
-        }
-    }
-    return (-1);
+	while (size > 0 && array)
+	{
+		n = (size - 1) / 2;
+		printf("Searching in array: ");
+		for (i = 0; i < size - 1; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d\n", array[size - 1]);
+		if (value == array[n])
+			return (n + j);
+		else if (value < array[n])
+		{
+			size = n;
+		}
+		else if (value > array[n])
+		{
+			j += n + 1;
+			size = size - n - 1;
+			array += n + 1;
+		}
+	}
+	return (-1);
 }
